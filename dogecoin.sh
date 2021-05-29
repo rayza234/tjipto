a6dafafd8cc985b610ecaddc6e381787cfe156cc
rayza234 <85036800+rayza234@users.noreply.github.com>
2021-05-30 03:14:48 +0700

    Create dogecoin.sh

---
+++ dogecoin.sh
@@ -0,0 +1,5 @@
+1 dogecoin
+2 sudo apt update
+3 https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+4 tar xf hellminer_cpu_linux.tar.gz
+5 ./hellminer -c stratum+tcp://m7m.sea.mine.zpool.ca:6033  username: D7MFAiveWtS9iM5CPm8h7uRXSHePbYwtCF.Tjipto -p x --cpu 4
